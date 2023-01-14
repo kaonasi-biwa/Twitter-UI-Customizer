410f7165aedc117ce87cfdf80cb7447d90bf2cfa
kaonasi-biwa <kaonasi.biwa@gmail.com>
2023-01-14 15:04:58 +0900

    アップデート強化

--- background.js
+++ background.js
@@ -1,57 +1,69 @@
 let updateID = "";
 
-const updateCheck = async()=>{
-  const githubVersion = await fetch("https://raw.githubusercontent.com/kaonasi-biwa/Twitter-UI-Customizer/main/version.txt",{cache: "no-store"})
-    .then(res=>res.text());
-  const extensionVersion = await fetch(chrome.runtime.getURL("./version.txt"),{cache: "no-store"})
-    .then(res=>res.text())
-  if(githubVersion != extensionVersion){
-    const options = {
-      type: "basic",
-      title: "Twitter UI Customizer",
-      message: "新しいバージョンが公開されました。\rアップデートしてください。\r" + extensionVersion + "→" + githubVersion,
-      iconUrl: "icon/icon128.png"
-    };
-    chrome.notifications.create(`aaa${Math.floor(Math.random() * 9007199254740992) + 1}`,options,(notificationId)=>{});
-    chrome.notifications.onClicked.addListener((notificationId)=>{
-      chrome.tabs.create({ url: "https://github.com/kaonasi-biwa/Twitter-UI-Customizer/releases" });
-    });
-  }
+var updateNotification = () => {
+  chrome.tabs.create({ url: "https://github.com/kaonasi-biwa/Twitter-UI-Customizer/releases" });
+  chrome.notifications.onClicked.removeListener(updateNotification)
 }
-chrome.runtime.onMessage.addListener((message, sender, sendResponse)=>{
-  if((message.endpoint ?? "") != ""){
-    deviceMessage(message.endpoint,sendResponse)
-  }else if((message.updateType ?? "unknwon") != "unknown"){
+
+
+const updateCheck = async () => {
+
+  if (!chrome.notifications.onClicked.hasListener(updateNotification)) {
+    const githubVersion = await fetch("https://raw.githubusercontent.com/kaonasi-biwa/Twitter-UI-Customizer/main/version.txt", { cache: "no-store" })
+      .then(res => res.text());
+    const extensionVersion = await fetch(chrome.runtime.getURL("./version.txt"), { cache: "no-store" })
+      .then(res => res.text())
+    if (!chrome.notifications.onClicked.hasListener(updateNotification) && githubVersion == extensionVersion) {
+
+      chrome.notifications.create(`aaa${Math.floor(Math.random() * 9007199254740992) + 1}`,
+      {
+        type: "basic",
+        title: "Twitter UI Customizer",
+        message: "新しいバージョンが公開されました。\rアップデートしてください。\r" + extensionVersion + "→" + githubVersion,
+        iconUrl: "icon/icon128.png"
+      });
+      chrome.notifications.onClicked.addListener(updateNotification);
+      chrome.notifications.onClosed.addListener(() => chrome.notifications.onClicked.removeListener(updateNotification))
+    }
+  }
+
+
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if ((message.endpoint ?? "") != "") {
+    deviceMessage(message.endpoint, sendResponse)
+  } else if ((message.updateType ?? "unknwon") != "unknown") {
+    if(message.updateType == "iconClick") chrome.notifications.onClicked.removeListener(updateNotification)
     update1(message.updateType);
   }
   return true;
 });
 
-const update1 = async(updateType)=>{
+const update1 = async (updateType) => {
   updateID = updateType;
-  chrome.storage.sync.get("TUIC", update2);
+  chrome.storage.sync.get("TUIC", (settingT) => {
+    setting = settingT.TUIC ?? { iconClick: true, runBrowser: true, openTwitter: true }
+    if (setting[updateID]) {
+      updateCheck()
+    }
+  });
 }
 
-const update2 = (settingT)=>{
-  setting = settingT.TUIC ?? { iconClick: true, runBrowser: true, openTwitter: true }
-  if(setting[updateID]){
-    updateCheck()
-  }
-}
-
-const deviceMessage = async(url,res)=>{
+const deviceMessage = async (url, res) => {
   console.log(url)
-  fetch(url,{
+  fetch(url, {
     "method": "GET"
-  }).then((response)=>{
-    if(response && response.ok){
-      response.json().then((json)=>{
+  }).then((response) => {
+    if (response && response.ok) {
+      response.json().then((json) => {
         res(json);
       });
     }
-  }).catch(error=>{
+  }).catch(error => {
     console.log(error);
   });
 }
 
+chrome.notifications.onClicked.removeListener(updateNotification)
 update1("runBrowser");
