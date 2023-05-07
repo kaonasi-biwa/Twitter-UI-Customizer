a5bba3ef696777a2fd995d1564d93d4e271e5f61
kaonasi_biwa <kaonasi.biwa@gmail.com>
2023-05-07 18:56:40 +0900

    今度こそ修正

--- scr/applyCSS.js
+++ scr/applyCSS.js
@@ -584,7 +584,7 @@ display:none !important;
  ` : ""}
 
  ${TUICPref.otherBoolSetting["bottomSpace"] ??  TUICData.defaultPref.otherBoolSetting.bottomSpace ? `
- article [aria-labelledby]{gap:0px !important;}
+ .${TUICLibrary.getClasses.getClass("TUIC_NONE_SPACE_BOTTOM_TWEET")}{margin-top:0px !important;}
  ` : ""}
 
 `;
