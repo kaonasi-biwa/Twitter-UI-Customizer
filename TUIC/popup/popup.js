485d2cdfbdfb7e1e7a4899ccc6cfe7fc7a3b1dee
nakasyou <syoutarou1103@outlook.jp>
2023-07-09 17:51:48 +0900

    表示を遅らせる

--- TUIC/popup/popup.js
+++ TUIC/popup/popup.js
@@ -17,12 +17,12 @@ window.onload = async ()=>{
     
     const $link5 = document.getElementById("link5")
     if (isFirefox) {
-        $link5.hidden = false;
         fetch("https://api.github.com/repos/kaonasi-biwa/Twitter-UI-Customizer/releases/latest", { cache: "no-store" })
             .then(res => res.json())
             .then(json => json.tag_name)
             .then(version => {
                 $link5.href = `https://github.com/kaonasi-biwa/Twitter-UI-Customizer/releases/download/${version}/Twitter_UI_Customizer_Firefox.xpi`
+                $link5.hidden = false;
             });
         } // Firefoxの場合のみ有効
     i18nApply()
