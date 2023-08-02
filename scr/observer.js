21db4ddcdd720ad80530968e5d56d35cb3596f44
kaonasi_biwa <kaonasi.biwa@gmail.com>
2023-08-02 20:10:25 +0900

    固定

--- scr/observer.js
+++ scr/observer.js
@@ -426,6 +426,14 @@ const TUICObserver = {
                     tweetElem.textContent = TUICLibrary.getI18n("XtoTwitter-PostToTweet-replayRangeDetail");
                     tweetElem.classList.add(TUICLibrary.getClasses.getClass("TUIC_TWEETREPLACE"));
                 }
+
+                tweetElem = document.querySelectorAll(`article path[d="M7 4.5C7 3.12 8.12 2 9.5 2h5C15.88 2 17 3.12 17 4.5v5.26L20.12 16H13v5l-1 2-1-2v-5H3.88L7 9.76V4.5z"]:not(.${TUICLibrary.getClasses.getClass("TUIC_TWEETREPLACE")} *)`);
+                if (tweetElem.length != 0) {
+                    for (const elem of tweetElem) {
+                        elem.parentElement.parentElement.parentElement.parentElement.querySelector(`[data-testid="socialContext"] > span`).textContent = TUICLibrary.getI18n("XtoTwitter-PostToTweet-pinnedTweet");
+                        elem.parentElement.parentElement.parentElement.parentElement.classList.add(TUICLibrary.getClasses.getClass("TUIC_TWEETREPLACE"));
+                    }
+                }
             }
         },
         twitterProPromotionBtn: function () {
