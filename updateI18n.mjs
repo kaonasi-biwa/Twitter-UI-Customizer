6af3767476d7de97e6c9d4a1bd03fe2c1cb7b36c
Kotone Fami <fami@ktnfm.com>
2023-08-19 10:25:14 +0900

    ti18n の自動生成の挙動に沿った形へ変更

--- updateI18n.mjs
+++ updateI18n.mjs
@@ -1,22 +1,18 @@
 import fetch from "node-fetch";
 import fs from "fs";
-console.log("Hello World!");
-console.log(JSON.stringify(["aaa", "bbb"]));
 
 (async () => {
     const locales = JSON.parse(fs.readFileSync("./i18n/_langList.json", "utf8"));
     const i18nObject = {};
     for (const elem of locales) {
-        console.log("aaaaa");
+        console.log(`Fetching i18n (${elem})...`);
         i18nObject[elem] = await (await fetch("https://raw.githubusercontent.com/fa0311/TwitterInternalAPIDocument/3f0ec1fb005f218a7eb60b580fd620541b1a9ad5/docs/json/i18n/" + elem + ".json")).json();
     }
     //https://github.com/fa0311/TwitterInternalAPIDocument/blob/3f0ec1fb005f218a7eb60b580fd620541b1a9ad5/docs/json/i18n/ja.json
-    console.log("aiueo");
-    console.log(fs.readFileSync("../Twitter-UI-Customizer/i18n/_officalTwitterI18n.json", "utf8"));
     const TUICI18ns = JSON.parse(fs.readFileSync("../Twitter-UI-Customizer/i18n/_officalTwitterI18n.json", "utf8"));
 
-    const TUICWriteI18n = {};
     for (const elem of locales) {
+        console.log(`Generating i18n (${elem})...`);
         let tmpObj = {};
         for (const elem2 in TUICI18ns) {
             if (TUICI18ns[elem2] == "g132f681") {
@@ -35,6 +31,6 @@ console.log(JSON.stringify(["aaa", "bbb"]));
                 tmpObj = { [elem2]: i18nObject[elem][TUICI18ns[elem2]], ...tmpObj };
             }
         }
-        fs.writeFileSync("./i18n/ti18n/" + elem + ".json", JSON.stringify(tmpObj));
+        fs.writeFileSync("./i18n/ti18n/" + elem + ".json", JSON.stringify(tmpObj, undefined, 4));
     }
 })();
