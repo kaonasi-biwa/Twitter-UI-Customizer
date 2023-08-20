d51b4c912a5bf061da08afeb575119d08835e2c3
typeling1578 <pub@typeling1578.dev>
2023-08-20 11:49:08 +0900

    if文とかnull演算子とかオプショナルチェーンとかを見直す & typoを修正

--- src/runningSafemode.js
+++ src/runningSafemode.js
@@ -1,5 +1,5 @@
 (async () => {
-    if (document.getElementById("react-root") != null && window.location.pathname == "/tuic/safemode") {
+    if (document.getElementById("react-root") && window.location.pathname == "/tuic/safemode") {
         if (document.querySelector("#safemode") == null) {
             await TUICLibrary.fetchI18n();
             document.querySelector("#react-root").style = "display:none !important;";
@@ -7,9 +7,7 @@
             safemode.id = "safemode";
             safemode.style = "height:100%;width:100%";
             document.querySelector("body").appendChild(safemode);
-            if (document.querySelector(".twitter_ui_customizer_css") != null) {
-                document.querySelector(".twitter_ui_customizer_css").remove();
-            }
+            document.querySelector(".twitter_ui_customizer_css")?.remove();
             const setTitle = () => {
                 document.title = TUICLibrary.getI18n("safemode-title");
                 window.setTimeout(setTitle, 5000);
