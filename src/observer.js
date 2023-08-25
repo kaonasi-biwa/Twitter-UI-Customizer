75d94c95dc6e0dfc986210fc24feb2306056e7d0
kaonasi_biwa <kaonasi.biwa@gmail.com>
2023-08-25 23:20:17 +0900

    プロフィールのいいねしたツイート・返信付き・メディア

--- src/observer.js
+++ src/observer.js
@@ -679,21 +679,53 @@ export const TUICObserver = {
         } else if (TUICPref.get("XToTwitter.XToTwitter")) {
             if (document.title == "X") {
                 document.title = "Twitter";
-                TUICObserver.headObserver.observe(document.querySelector("title"), {
-                    characterData: true,
-                    childList: true,
-                    subtree: true,
-                    attributes: true,
-                });
             } else if (window.location.pathname.includes("/i/timeline") || window.location.pathname.includes("/compose/tweet")) {
                 TUICObserver.headObserver.disconnect();
                 document.title = document.title.startsWith("(") ? document.title.match(/\(\d*\)/) ?? "" : "" + TUICI18N.get("XtoTwitter-PostToTweet-tweetNotificationsTitle") + " / Twitter";
-                TUICObserver.headObserver.observe(document.querySelector("title"), {
-                    characterData: true,
-                    childList: true,
-                    subtree: true,
-                    attributes: true,
-                });
+            } else if (window.location.pathname.endsWith("/with_replies") && !window.location.pathname.includes("/status/")) {
+                TUICObserver.headObserver.disconnect();
+                const titleInfo = document.title.match(
+                    new RegExp(TUICI18N.get("XtoTwitter-PostToTweet-profile-postsWithReplies-latest").replaceAll("&quot;", '"').replaceAll("(", "\\(").replaceAll(")", "\\)").replace("{fullName}", "(.*)").replace("{screenName}", "(.*)")),
+                ); /*/Xユーザーの(.*)さん: 「(.*)」/*/
+                if (!titleInfo || titleInfo.length <= 1) {
+                    document.title = document.title.replace(/(.*)\/ X/, "$1/ Twitter");
+                } else {
+                    document.title =
+                        (document.title.startsWith("(") ? document.title.match(/\(\d*\)/) + " " : "") +
+                        TUICI18N.get("XtoTwitter-PostToTweet-profile-postsWithReplies-old")
+                            .replaceAll("&quot;", '"')
+                            .replace(`{fullName}`, titleInfo[1])
+                            .replace("{screenName}", titleInfo[2])
+                            .replace(/(.*)\/ X(」|")/, "$1 / Twitter");
+                }
+            } else if (window.location.pathname.endsWith("/media") && !window.location.pathname.includes("/status/")) {
+                TUICObserver.headObserver.disconnect();
+                const titleInfo = document.title.match(new RegExp(TUICI18N.get("XtoTwitter-PostToTweet-profile-media-latest").replaceAll("&quot;", '"').replaceAll("(", "\\(").replaceAll(")", "\\)").replace("{fullName}", "(.*)").replace("{screenName}", "(.*)"))); /*/Xユーザーの(.*)さん: 「(.*)」/*/
+                if (!titleInfo || titleInfo.length <= 1) {
+                    document.title = document.title.replace(/(.*)\/ X/, "$1/ Twitter");
+                } else {
+                    document.title =
+                        (document.title.startsWith("(") ? document.title.match(/\(\d*\)/) + " " : "") +
+                        TUICI18N.get("XtoTwitter-PostToTweet-profile-media-old")
+                            .replaceAll("&quot;", '"')
+                            .replace(`{fullName}`, titleInfo[1])
+                            .replace("{screenName}", titleInfo[2])
+                            .replace(/(.*)\/ X(」|")/, "$1 / Twitter");
+                }
+            } else if (window.location.pathname.endsWith("/likes") && !window.location.pathname.includes("/status/")) {
+                TUICObserver.headObserver.disconnect();
+                const titleInfo = document.title.match(new RegExp(TUICI18N.get("XtoTwitter-PostToTweet-profile-likes-latest").replaceAll("&quot;", '"').replaceAll("(", "\\(").replaceAll(")", "\\)").replace("{fullName}", "(.*)").replace("{screenName}", "(.*)"))); /*/Xユーザーの(.*)さん: 「(.*)」/*/
+                if (!titleInfo || titleInfo.length <= 1) {
+                    document.title = document.title.replace(/(.*)\/ X/, "$1/ Twitter");
+                } else {
+                    document.title =
+                        (document.title.startsWith("(") ? document.title.match(/\(\d*\)/) + " " : "") +
+                        TUICI18N.get("XtoTwitter-PostToTweet-profile-likes-old")
+                            .replaceAll("&quot;", '"')
+                            .replace(`{fullName}`, titleInfo[1])
+                            .replace("{screenName}", titleInfo[2])
+                            .replace(/(.*)\/ X(」|")/, "$1 / Twitter");
+                }
             } else if (window.location.pathname.includes("/status/")) {
                 /**/
                 TUICObserver.headObserver.disconnect();
@@ -701,14 +733,13 @@ export const TUICObserver = {
                 if (!titleInfo || titleInfo.length <= 1) {
                     document.title = document.title.replace(/(.*)\/ X/, "$1/ Twitter");
                 } else {
-                    document.title = document.title.startsWith("(")
-                        ? document.title.match(/\(\d*\)/) ?? ""
-                        : "" +
-                          TUICI18N.get("XtoTwitter-PostToTweet-titlePeopleTweeted")
-                              .replaceAll("&quot;", '"')
-                              .replace(`{fullName}`, titleInfo[1])
-                              .replace("{tweetText}", titleInfo[2])
-                              .replace(/(.*)\/ X(」|")/, "$1 / Twitter");
+                    document.title =
+                        (document.title.startsWith("(") ? document.title.match(/\(\d*\)/) + " " : "") +
+                        TUICI18N.get("XtoTwitter-PostToTweet-titlePeopleTweeted")
+                            .replaceAll("&quot;", '"')
+                            .replace(`{fullName}`, titleInfo[1])
+                            .replace("{tweetText}", titleInfo[2])
+                            .replace(/(.*)\/ X(」|")/, "$1 / Twitter");
                 } /**/
             } else if (document.title.endsWith(" / X")) {
                 document.title = document.title.replace(/(.*)\/ X/, "$1/ Twitter");
