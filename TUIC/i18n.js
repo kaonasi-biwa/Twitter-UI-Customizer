ed2c97f00eca324b393de766e4aefc6e2a898882
Kotone Fami <fami@ktnfm.com>
2023-08-26 10:39:14 +0900

    ソースコードを src に集約

--- TUIC/i18n.js
+++
@@ -1,5 +0,0 @@
-function i18nApply() {
-    for (const elem of document.querySelectorAll(".i18n")) {
-        elem.textContent = chrome.i18n.getMessage(elem.getAttribute("i18n-id") ?? "");
-    }
-}
