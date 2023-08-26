2f4fd8113814733e9bb50bed13d01faf5c61ee67
Kotone Fami <fami@ktnfm.com>
2023-08-26 10:10:32 +0900

    引数エラーを吐くように調整

--- manifestChange.mjs
+++ manifestChange.mjs
@@ -4,11 +4,18 @@ import fs from "fs/promises";
     // CLI引数または_langList.jsonファイルからロケールを取得
     const config = JSON.parse(await fs.readFile("./manifestConfigs.json", "utf8"));
 
+    const target = process.argv[2];
+
+    if (target === undefined) {
+        console.error(`Error: Please specify a platform for generate. (${Object.keys(config).filter(k => k !== "common").join(", ")})`);
+        process.exit(1);
+    }
+
     let output = {};
-    if (process.argv[2] == "chromeCRX") {
+    if (target == "chromeCRX") {
         output = Object.assign(config.common, config.chrome, config.chromeCRX);
     } else {
-        output = Object.assign(config.common, config[process.argv[2]]);
+        output = Object.assign(config.common, config[target]);
     }
 
     await fs.writeFile("./manifest.json", JSON.stringify(output, undefined, 4));
