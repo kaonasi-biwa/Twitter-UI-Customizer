3192677c38ae9539666b5a717af643db6c887a4a
kaonasi_biwa <kaonasi.biwa@gmail.com>
2023-11-17 20:47:59 +0900

    バグ修正

--- src/content/observer.js
+++ src/content/observer.js
@@ -191,7 +191,7 @@ export const TUICObserver = {
         },
         showLinkCardInfo: function () {
             if (TUICPref.get("otherBoolSetting.showLinkCardInfo")) {
-                for (const infoCardElem of document.querySelectorAll(`article:not(.TUICDidInfoArticle) [data-testid="card.layoutLarge.media"]  a > div+div`)) {
+                for (const infoCardElem of document.querySelectorAll(`article:not(.TUICDidInfoArticle) [data-testid="card.layoutLarge.media"]  a[aria-label] > div+div`)) {
                     let elem = infoCardElem;
                     while (elem.tagName != "ARTICLE") {
                         elem = elem.parentNode;
