3e342856264988f1a16822cd7971bbcc9cf998aa
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2023-12-17 11:08:01 +0900

    ESLintのconfigを改善

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,59 @@ import prettier from "eslint-config-prettier";
 
 export default [
     {
+        ignores: ["dist/**", "node_modules/**"],
+    },
+    {
+        files: ["**/*.{js,ts,vue}"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                i18nApply: true,
+                process: true,
+            },
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: "module",
+            },
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...prettier.rules,
+            indent: 0,
+            semi: ["error", "always"],
+            "prefer-const": ["error"],
+            "linebreak-style": 0,
+            "no-unused-vars": 0,
+            "no-empty": 0,
+            "no-unsafe-option-chaining": 0,
+        },
+    },
+    {
+        files: ["**/*.{ts,vue}"],
+        languageOptions: {
+            parser: vue_parser,
+            parserOptions: {
+                parser: typescript_parser,
+            },
+        },
+        plugins: {
+            "@typescript-eslint": typescript_eslint,
+            vue: vue_eslint,
+        },
+        rules: {
+            ...typescript_eslint.configs.recommended.rules,
+            ...vue_eslint.configs["vue3-recommended"].rules,
+            "no-undef": 0,
+            "@typescript-eslint/no-unused-vars": 0,
+            "vue/no-unused-vars": 0,
+            "vue/no-unused-components": 0,
+            "vue/attribute-hyphenation": 0,
+            "vue/attributes-order": 0,
+            "vue/require-prop-types": 0,
+            "vue/prop-name-casing": 0,
+        },
+    },
+    {
         files: ["src/**"],
         languageOptions: {
             globals: {
@@ -24,47 +77,6 @@ export default [
             globals: {
                 ...globals.node,
             },
-        },
-    },
-    {
-        files: ["**/*.{js,ts,vue}"],
-        ignores: ["dist/**", "node_modules/**"],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                i18nApply: true,
-                process: true,
-            },
-            parser: vue_parser,
-            parserOptions: {
-                ecmaVersion: 2022,
-                sourceType: "module",
-                parser: typescript_parser,
-            },
-        },
-        plugins: {
-            "@typescript-eslint": typescript_eslint,
-            vue: vue_eslint,
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            ...typescript_eslint.configs.recommended.rules,
-            ...vue_eslint.configs["vue3-recommended"].rules,
-            ...prettier.rules,
-            indent: 0,
-            semi: ["error", "always"],
-            "prefer-const": ["error"],
-            "linebreak-style": 0,
-            "no-unused-vars": 0,
-            "@typescript-eslint/no-unused-vars": 0,
-            "vue/no-unused-vars": 0,
-            "vue/no-unused-components": 0,
-            "vue/attribute-hyphenation": 0,
-            "vue/attributes-order": 0,
-            "vue/require-prop-types": 0,
-            "vue/prop-name-casing": 0,
-            "no-empty": 0,
-            "no-unsafe-option-chaining": 0,
         },
     },
 ];
