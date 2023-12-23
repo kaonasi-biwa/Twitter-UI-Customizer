8031bc8e3e30f2a771f956ed7a0d0a6cfb76eaaf
kaonasi_biwa <kaonasi.biwa@gmail.com>
2023-12-23 18:11:44 +0900

    Version Up

--- manifest.config.js
+++ manifest.config.js
@@ -3,7 +3,7 @@ export default {
     common: {
         name: "__MSG_extensionName__",
         description: "__MSG_extensionDescription__",
-        version: "3.9.1",
+        version: "4.0.0",
         default_locale: "ja",
         content_scripts: [
             {
