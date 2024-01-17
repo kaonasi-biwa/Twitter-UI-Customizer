c2420f91d22a2e73acd2cc838f0766fd07736cdc
kaonasi_biwa <kaonasi.biwa@gmail.com>
2024-01-17 23:36:22 +0900

    getI18nを抹消

--- src/background.ts
+++ src/background.ts
@@ -32,8 +32,6 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type == "update") {
         if (message.updateType == "iconClick") browser.notifications.onClicked.removeListener(updateNotification);
         update1(message.updateType);
-    } else if (message.type == "getI18n") {
-        returnI18n(sendResponse);
     }
     return true;
 });
@@ -55,34 +53,5 @@ const update1 = async (updateType) => {
     }
 };
 
-const returnI18n = async (res) => {
-    const waitFunc = () => {
-        if (loadedI18n) {
-            res(i18nObject);
-        } else {
-            browser.alarms.create({ when: Date.now() + 250 });
-        }
-    };
-    browser.alarms.onAlarm.addListener(waitFunc);
-    waitFunc();
-};
-
-const getI18n = async () => {
-    i18nObject = {};
-    const langList = await fetch(browser.runtime.getURL("./i18n/_langList.json"), { cache: "no-store" }).then((res) => res.json());
-    for (const elem of langList) {
-        i18nObject[elem] = Object.assign(
-            await fetch(browser.runtime.getURL(`./i18n/${elem}.json`), {
-                cache: "no-store",
-            }).then((res) => res.json()),
-            await fetch(browser.runtime.getURL(`./i18n/ti18n/${elem}.json`), {
-                cache: "no-store",
-            }).then((res) => res.json()),
-        );
-    }
-    loadedI18n = true;
-};
-
 browser.notifications.onClicked.removeListener(updateNotification);
 update1("runBrowser");
-getI18n();
