7e3ceda0c888f45881cc7061eb69c84fe30f9995
kaonasi_biwa <kaonasi.biwa@gmail.com>
2024-02-04 12:04:59 +0900

    コメントつけてみた

--- src/content/index.ts
+++ src/content/index.ts
@@ -14,41 +14,56 @@ import { initIconObserverFunction } from "./modules/observer/functions/changeIco
 import { titleObserverFunction } from "./modules/observer/titleObserver.ts";
 
 (async () => {
+    // I18Nを取得
     await TUICI18N.fetch();
 
+    // Twitterの親要素を待機
     await TUICLibrary.waitForElement("#react-root");
 
+    // 前起動時のTUICの要素・Classが残っていればすべて削除
+    TUICLibrary.getClasses.deleteClasses();
     for (const elem of document.querySelectorAll(".TUICOriginalContent")) {
         elem.remove();
     }
+
+    // アップデート通知
+    chrome.runtime.sendMessage({
+        type: "update",
+        updateType: "openTwitter",
+    });
+
+    // 起動メッセージ
     console.log(
         `%cTwitter UI Customizer${isSafemode ? " (Safe Mode)" : ""}%cby kaonasi_biwa\n\nTwitter を思いのままに。⧸ Language: ${TUICI18N.get("@JapaneseLanguageName")}`,
         `font-family: system-ui, -apple-system, sans-serif, monospace; font-size: 1.2em; font-weight: bold; text-align: center; background: ${isSafemode ? "#5a9e1b" : "#1da1f2"}; color: #ffffff; padding: 0.5em 2em; margin-top: 0.5em; margin-left: 0.5em;`,
         `font-family: system-ui, -apple-system, sans-serif, monospace; margin: 0.5em; color: ${isSafemode ? "#5a9e1b" : "#1da1f2"};`,
     );
 
-    // 旧バージョンからのアップデート
+    // 旧バージョンからのPrefのアップデート
     await TUICPref.updatePref();
 
+    // CSSの適用
     addCssElement();
     applyDataCss();
     applyCustomIcon();
+
+    // 起動時のTwitterアイコンを変更
     if (document.querySelector(`#placeholder > svg`)) {
         initIconObserverFunction();
     }
-    TUICLibrary.getClasses.deleteClasses();
+
+    // タイトル変更のためのObserver
     await TUICLibrary.waitForElement("title");
     titleObserverFunction();
-    chrome.runtime.sendMessage({
-        type: "update",
-        updateType: "openTwitter",
-    });
 
+    // TLUI用のObserver
     startTluiObserver();
 
+    // メインのObserver
     (TUICObserver.target = document.querySelector("body")), TUICObserver.observer.observe(TUICObserver.target, TUICObserver.config);
     TUICObserver.observerFunction();
 
+    // フォントサイズ変更の検出のためのObserver
     const bodyAttributeObserver = new MutationObserver(applySystemCss);
     bodyAttributeObserver.observe(document.querySelector("body"), {
         childList: false,
@@ -56,5 +71,6 @@ import { titleObserverFunction } from "./modules/observer/titleObserver.ts";
         attributes: true,
     });
 
+    // セーフモード
     if (isSafemode) runSafemode();
 })();
