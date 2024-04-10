020ad81c2bbbfdc4c90f6f7269b360106f7c9bb6
kaonasi_biwa <kaonasi.biwa@gmail.com>
2024-04-10 21:48:31 +0900

    固定ツイートの文字列が正常に置き換えられない問題を修正

--- src/content/modules/observer/functions/replacePostWithTweet.ts
+++ src/content/modules/observer/functions/replacePostWithTweet.ts
@@ -312,7 +312,7 @@ export function replacePost() {
 
         // 固定ツイートの「固定」表示
         for (const elem of getNotReplacedElements('[data-testid="tweet"] path[d="M7 4.5C7 3.12 8.12 2 9.5 2h5C15.88 2 17 3.12 17 4.5v5.26L20.12 16H13v5l-1 2-1-2v-5H3.88L7 9.76V4.5z"]'))
-            elem.hasClosestSelector(`[data-testid="socialContext"] > span`).textContent = TUICI18N.get("XtoTwitter-PostToTweet-pinnedTweet");
+            elem.hasClosestSelector(`[data-testid="socialContext"]`).textContent = TUICI18N.get("XtoTwitter-PostToTweet-pinnedTweet");
 
         // 「{user}さんがリツイートしました」もしくは「リツイート済み」
         for (const elem of getNotReplacedElements(
