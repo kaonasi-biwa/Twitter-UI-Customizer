0a52ebf100e3b06c89d92a6ce01712c60eea33d6
kaonasi_biwa <kaonasi.biwa@gmail.com>
2024-05-17 15:00:09 +0900

    軽く説明

--- src/content/printPref.ts
+++ src/content/printPref.ts
@@ -1,6 +1,7 @@
 if (location.href === "https://twitter.com//") {
     console.log("aiueo");
     //document.write("aaa");
+    alert("エクスポートされた設定、CSSの順で表示されます。それぞれコピーしてx.comで保存してください。");
     alert(localStorage.getItem("TUIC"));
     alert(localStorage.getItem("TUIC_CSS"));
 }
