c5f38060e1289b1126cd69d92c89abf10971a399
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-11-10 15:12:55 +0900

    破壊的変更を含む?依存関係の更新

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, presetUno } from "unocss";
-import type { Theme } from "unocss/preset-uno";
 
-export default defineConfig<Theme>({
+export default defineConfig({
     presets: [presetUno()],
 });
