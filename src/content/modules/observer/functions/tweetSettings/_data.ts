8a007b43448a7f069470ba400a0c2baf3a6d5ee9
kaonasi_biwa <kaonasi.biwa@gmail.com>
2024-12-13 21:32:12 +0900

    (おそらく)Grokボタンが正常に非表示にならない問題を修正

--- src/content/modules/observer/functions/tweetSettings/_data.ts
+++ src/content/modules/observer/functions/tweetSettings/_data.ts
@@ -11,7 +11,7 @@ export const ButtonUnderTweetSelectors = {
     "reply-button": '[data-testid$="reply"]:not([data-testid*="UserAvatar-Container-"])',
     "retweet-button": '[data-testid$="retweet"]:not([data-testid*="UserAvatar-Container-"])',
     "like-button": '[data-testid$="like"]:not([data-testid*="UserAvatar-Container-"])',
-    "share-button": '[aria-haspopup="menu"]:not([data-testid="retweet"]):not([data-testid="unretweet"])',
+    "share-button": '[aria-haspopup="menu"]:not([data-testid="retweet"]):not([data-testid="unretweet"]) [d="M12 2.59l5.7 5.7-1.41 1.42L13 6.41V16h-2V6.41l-3.3 3.3-1.41-1.42L12 2.59zM21 15l-.02 3.51c0 1.38-1.12 2.49-2.5 2.49H5.5C4.11 21 3 19.88 3 18.5V15h2v3.5c0 .28.22.5.5.5h12.98c.28 0 .5-.22.5-.5L19 15h2z"]',
     tweet_analytics: '[href$="/analytics"],[d="M8.75 21V3h2v18h-2zM18 21V8.5h2V21h-2zM4 21l.004-10h2L6 21H4zm9.248 0v-7h2v7h-2z"]',
     boolkmark: `[data-TUICButton="bookmark"],[data-testid="bookmark"]:not([data-testid*="UserAvatar-Container-"]),[data-testid="removeBookmark"]:not([data-testid*="UserAvatar-Container-"])`,
     "url-copy": `[data-TUICButton="url-copy"]`,
