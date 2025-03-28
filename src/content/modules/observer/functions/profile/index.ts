43836f43da2ca4958d9032e0aa7daf063d447e96
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2025-03-28 15:17:15 +0900

    「返信」タブを「ツイートと返信」に戻せる機能を追加

--- src/content/modules/observer/functions/profile/index.ts
+++ src/content/modules/observer/functions/profile/index.ts
@@ -1,5 +1,7 @@
+import { getPref } from "@content/modules/pref";
 import { followersList } from "./followersListButton";
 import { profileInitialTab } from "./initProfileTab";
+import { TUICI18N } from "@content/modules/i18n";
 
 export function profileModify(){
     // フォロワー一覧のボタンについての処理
@@ -7,4 +9,12 @@ export function profileModify(){
 
     // プロフィール画面を最初に開いたときのタブについての処理
     profileInitialTab()
+
+    // 「返信」タブを「ツイートと返信」の名称に戻す
+    if(getPref("profileSetting.tabs.changeNameReplies")) {
+        const repliesTabElement = document.querySelector(`[role="navigation"] [href$="/with_replies"] span`);
+        if (repliesTabElement) {
+            repliesTabElement.textContent = TUICI18N.get("profileSetting-changeName-replies-oldName");
+        }
+    }
 }
