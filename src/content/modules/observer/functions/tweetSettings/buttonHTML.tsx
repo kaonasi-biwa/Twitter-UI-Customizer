51c029d91b18b76f52fe05c38c07aea67e8fab7a
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2025-04-14 21:09:17 +0900

    「ダイレクトメッセージで送信」ボタンが正常に動かなかった問題を修正

--- src/content/modules/observer/functions/tweetSettings/buttonHTML.tsx
+++ src/content/modules/observer/functions/tweetSettings/buttonHTML.tsx
@@ -214,7 +214,8 @@ export const tweetButtonData: Record<string, {
             );
         },
         clickEvent: async (data: ArticleInfomation) => {
-            data.elements.buttonBarBase.querySelector<HTMLButtonElement>(ButtonUnderTweetSelectors["share-button"]).click();
+            console.log(data.elements.buttonBarBase.querySelector<HTMLButtonElement>(ButtonUnderTweetSelectors["share-button"]))
+            data.elements.buttonBarBase.querySelector<HTMLButtonElement>(ButtonUnderTweetSelectors["share-button"]).closest("button").click();
             (
                 await waitForElement<HTMLButtonElement>(
                     `[role="menu"] [role="menuitem"] [d="M1.998 5.5c0-1.381 1.119-2.5 2.5-2.5h15c1.381 0 2.5 1.119 2.5 2.5v13c0 1.381-1.119 2.5-2.5 2.5h-15c-1.381 0-2.5-1.119-2.5-2.5v-13zm2.5-.5c-.276 0-.5.224-.5.5v2.764l8 3.638 8-3.636V5.5c0-.276-.224-.5-.5-.5h-15zm15.5 5.463l-8 3.636-8-3.638V18.5c0 .276.224.5.5.5h15c.276 0 .5-.224.5-.5v-8.037z"]:not(.TUIC_sendDM)`,
