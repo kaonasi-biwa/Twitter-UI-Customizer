2a69d594f8198795b63b04aff572f2a30c13f201
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2025-05-08 18:05:33 +0900

    Version Up

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ export default {
     common: {
         name: "__MSG_extensionName__",
         description: "__MSG_extensionDescription__",
-        version: "5.3.0",
+        version: "5.3.1",
         default_locale: "ja",
         content_scripts: [
             {
