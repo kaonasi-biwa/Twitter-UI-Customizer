59f3938fa7df39a3ba73ea688148da80a76ff276
Hibi_10000 <Hibi-10000@outlook.jp>
2025-08-08 22:19:14 +0900

    rightSidebarTextsを今のTwitterを元に変更

--- src/content/modules/observer/functions/rightSidebarTexts.tsx
+++ src/content/modules/observer/functions/rightSidebarTexts.tsx
@@ -10,57 +10,61 @@ import { DivBoxComponent } from "@shared/tlui/components/DivBox";
 
 const elem = (): JSX.Element => {
     return (
-        <a
-            href="https://twitter.com/i/display"
-            id="TUICDisplayRightSidebar"
-            dir="ltr"
-            role="link"
-            class={`TUICOriginalContent css-1rynq56 min-w-[0px] wrap-break-word font-tw font-normal my-[2px] cursor-pointer ${fontSizeClass(
-                "text-[12px] leading-[14px] pr-[11px]",
-                "text-[12px] leading-[15px] pr-[11px]",
-                "text-[13px] leading-[16px] pr-[12px]",
-                "text-[14px] leading-[18px] pr-[13px]",
-                "text-[16px] leading-[19px] pr-[14px]",
-            )}`}
-            style={{
-                "text-overflow": "unset",
-                color: `rgb(${backgroundColorClass<string>("113, 118, 123", "139, 152, 165", "83, 100, 113")})`,
-                "padding-top": "1.5px",
-            }}
-            onClick={(e) => {
-                e.preventDefault();
-                setTimeout(async () => {
-                    await waitForElement("#layers");
-                    const dialog = new Dialog(TUICI18N.get("common-displaySetting"));
-                    dialog.contentWidth = "50vw";
-                    //dialog.fitContentWidth = true;
-                    dialog
-                        .addComponents([
-                            new ButtonComponent(TUICI18N.get("common-close"), () => {
-                                dialog.close();
-                            }),
-                            new DivBoxComponent({ id: "TUICOriginalDisplaySetting" }),
-                            new ButtonComponent(TUICI18N.get("common-close"), () => {
-                                dialog.close();
-                            }),
-                        ])
-                        .open();
-                }, 150);
-                /*
-                const moreMenu = document.querySelector<HTMLElement>(`[data-testid="AppTabBar_More_Menu"] > div > div`);
-                if (document.querySelector(`[role="menu"]`) == null) moreMenu.click();
-                setTimeout(async () => {
-                    //document.querySelector<HTMLElement>(`:is([role="group"],[data-testid="Dropdown"]) [data-testid="settingsAndSupport"]`).click();
-                    (await waitForElement<HTMLAnchorElement>(`[href="/settings"]`))[0].click();
-                    (await waitForElement<HTMLAnchorElement>(`[href="/settings/accessibility_display_and_languages"]`))[0].click();
-                    (await waitForElement<HTMLAnchorElement>(`[href="/settings/display"]`))[0].click();
-                }, 150);*/
-            }}
+        <div
+            class={`css-175oi2r cursor-pointer self-center my-[2px] ${fontSizeClass(
+                "pr-[11px]", "pr-[11px]", "pr-[12px]", "pr-[13px]", "pr-[14px]",
+            )} TUICOriginalContent`}
         >
-            <span class="css-1qaijid min-w-[0px] wrap-break-word font-tw" style={{ "text-overflow": "unset" }}>
-                {TUICI18N.get("common-displaySetting")}
-            </span>
-        </a>
+            <a
+                href="https://twitter.com/i/display"
+                id="TUICDisplayRightSidebar"
+                dir="ltr"
+                role="link"
+                class={`css-146c3p1 min-w-[0px] text-align-inherit wrap-break-word font-tw2 ${fontSizeClass(
+                    "text-[12px] leading-[14px]",
+                    "text-[12px] leading-[15px]",
+                    "text-[13px] leading-[16px]",
+                    "text-[14px] leading-[18px]",
+                    "text-[16px] leading-[19px]",
+                )} font-normal`}
+                style={{
+                    color: `rgb(${backgroundColorClass<string>("113, 118, 123", "139, 152, 165", "83, 100, 113")})`,
+                }}
+                onClick={(e) => {
+                    e.preventDefault();
+                    setTimeout(async () => {
+                        await waitForElement("#layers");
+                        const dialog = new Dialog(TUICI18N.get("common-displaySetting"));
+                        dialog.contentWidth = "50vw";
+                        //dialog.fitContentWidth = true;
+                        dialog
+                            .addComponents([
+                                new ButtonComponent(TUICI18N.get("common-close"), () => {
+                                    dialog.close();
+                                }),
+                                new DivBoxComponent({ id: "TUICOriginalDisplaySetting" }),
+                                new ButtonComponent(TUICI18N.get("common-close"), () => {
+                                    dialog.close();
+                                }),
+                            ])
+                            .open();
+                    }, 150);
+                    /*
+                    const moreMenu = document.querySelector<HTMLElement>(`[data-testid="AppTabBar_More_Menu"] > div > div`);
+                    if (document.querySelector(`[role="menu"]`) == null) moreMenu.click();
+                    setTimeout(async () => {
+                        //document.querySelector<HTMLElement>(`:is([role="group"],[data-testid="Dropdown"]) [data-testid="settingsAndSupport"]`).click();
+                        (await waitForElement<HTMLAnchorElement>(`[href="/settings"]`))[0].click();
+                        (await waitForElement<HTMLAnchorElement>(`[href="/settings/accessibility_display_and_languages"]`))[0].click();
+                        (await waitForElement<HTMLAnchorElement>(`[href="/settings/display"]`))[0].click();
+                    }, 150);*/
+                }}
+            >
+                <span class="css-1jxf684 min-w-[0px] text-align-inherit wrap-break-word font-inherit">
+                    {TUICI18N.get("common-displaySetting")}
+                </span>
+            </a>
+        </div>
     );
 };
 
