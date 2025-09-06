b5fbbb07b5c025ef0e047d7ffd98045ee0fa5bba
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2025-09-06 21:38:23 +0900

    ツイート上ボタンのツイートを削除ボタンが動作しない問題を修正

--- src/content/modules/observer/functions/tweetSettings/tweetTopButtons.tsx
+++ src/content/modules/observer/functions/tweetSettings/tweetTopButtons.tsx
@@ -133,8 +133,9 @@ const _data = {
             const elem = _data.buttonElement._base(
                 "delete",
                 () => (<path d="M16 6V4.5C16 3.12 14.88 2 13.5 2h-3C9.11 2 8 3.12 8 4.5V6H3v2h1.06l.81 11.21C4.98 20.78 6.28 22 7.86 22h8.27c1.58 0 2.88-1.22 3-2.79L19.93 8H21V6h-5zm-6-1.5c0-.28.22-.5.5-.5h3c.27 0 .5.22.5.5V6h-4V4.5zm7.13 14.57c-.04.52-.47.93-1 .93H7.86c-.53 0-.96-.41-1-.93L6.07 8h11.85l-.79 11.07zM9 17v-6h2v6H9zm4 0v-6h2v6h-2z" class="TUIC_DeleteButton"></path>),
-                !info.option.isMe
+                info.option.isMe
                     ? async () => {
+                        console.log("a")
                         moremenu.click();
                         (
                             await waitForElement<HTMLButtonElement>(
