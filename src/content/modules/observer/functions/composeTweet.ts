5d71de464137d5f68f2351fcad87a6cb2fe6b9ab
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2025-11-10 22:44:23 +0900

    OFFにできなくなってた

--- src/content/modules/observer/functions/composeTweet.ts
+++ src/content/modules/observer/functions/composeTweet.ts
@@ -1,8 +1,9 @@
+import { getPref } from "@content/modules/pref";
 import { processElement, waitForElement } from "@content/modules/utils/controlElements";
 import { ProcessedClass } from "@shared/sharedData";
 
 export function composetweet(){
-    if(location.pathname === "/compose/post" && document.querySelector(`[data-testid="tweetButton"]:not(.${ProcessedClass})`)){
+    if(getPref("composetweet.remainOpened") && location.pathname === "/compose/post" && document.querySelector(`[data-testid="tweetButton"]:not(.${ProcessedClass})`)){
         const composeTweetButton = document.querySelector<HTMLButtonElement>(`[data-testid="tweetButton"]`);
         composeTweetButton.addEventListener("click", async () => {
             if(composeTweetButton.disabled) return;
